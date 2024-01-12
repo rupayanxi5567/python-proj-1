fd68d83469cf2b0c519d1398530c5243dd678ce3
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-12 08:25:46 +0000

    insta bot project done

---
+++ Python Projects-2/insta_bot.py
@@ -0,0 +1,14 @@
+from instabot import Bot
+
+bot = Bot()
+bot.login(username = "sportsalert24x7", password = "Bulai@2004")
+
+# bot.follow("sugar_high_love_stoned__1377") #follows a person
+# bot.upload_photo("pc file path", caption="i hate love stories") #uploads a photo
+# bot.unfollow("sugar_high_love_stoned__1377") #unfollows a person
+# bot.send_message("Hii dear", ["sugar_high_love_stoned__1377", "honey_comb1377"]) #dm a person
+
+# followers = bot.get_user_followers("sportsalert24x7") #gives info of all the follwers
+# for x in followers:
+#     print(bot.get_user_info(x))
+
